10389aae69603fa88171cec3f678f8cf01c15673
Matt <mpbochenek86@gmail.com>
2018-08-07 09:54:17 +0100

    Create books component and books array

---
+++ src/Books.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+
+class Books extends Component {
+  render() {
+    return (
+      <ol className='books-grid'>
+        {this.props.books.map((book) => (
+          <li key={book.id}>
+          <div className="book">
+            <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+              </div>
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.author}</div>
+            </div>
+          </li>
+        ))}
+      </ol>
+    )
+  }
+}
+
+export default Books
