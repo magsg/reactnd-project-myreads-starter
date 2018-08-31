d9f9fdeee5fbd0aa32e00bac096b18f67566f891
Matt <mpbochenek86@gmail.com>
2018-08-31 15:54:15 +0100

    add books api

--- src/App.js
+++ src/App.js
@@ -2,60 +2,18 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Books from './Books'
+import * as BooksAPI from './BooksAPI'
 
-const books = [
-      {
-        "id": 1,
-        "cover": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-        "title": "To Kill a Mockingbird",
-        "author": "Harper Lee"
-      },
 
-      {
-        "id": 2,
-        "cover": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-        "title": "Ender's Game",
-        "author": "Orson Scott Card"
-      },
-
-      {
-        "id": 3,
-        "cover": "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-        "title": "1776",
-        "author": "David McCullough"
-      },
-
-      {
-        "id": 4,
-        "cover": "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-        "title": "Harry Potter and the Sorcerer's Stone",
-        "author": "J.K. Rowling"
-      },
-
-      {
-        "id": 5,
-        "cover": "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-        "title": "The Hobbit",
-        "author": "J.R.R. Tolkien"
-      },
-
-      {
-        "id": 6,
-        "cover": "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
-        "title": "Oh, the Places You'll Go!",
-        "author": "Seuss"
-      },
-
-      {
-        "id": 7,
-        "cover": "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
-        "title": "The Adventures of Tom Sawyer",
-        "author": "Mark Twain"
-      }
-    ]
 
 class BooksApp extends React.Component {
   state = {
+    books: [],
+    current: [],
+    want: [],
+    read: [],
+    // shelf: '',
+
 
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -66,9 +24,76 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
 
+
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books)=> {
+      this.setState({books})
+      this.setState((state) => ({
+        current: state.books.filter((b) => b.shelf === "currentlyReading")
+      }))
+      this.setState((state) => ({
+        want: state.books.filter((b) => b.shelf === "wantToRead")
+      }))
+      this.setState((state) => ({
+        read: state.books.filter((b) => b.shelf === "read")
+      }))
+    })
+
+  }
+
+
+
+updateBook = (book, shelf) => {
+
+  // this.setState((state) => ({
+  //   books: this.state.books, {book: shelf}
+  // }))
+  if(shelf === "currentlyReading") {
+  let nb = this.state.current.concat(book)
+  this.setState({current: nb})
+
+  
+  this.setState((state) => ({
+    current: state.books.filter((b) => b.id === book.id && b.shelf === "currentlyReading")
+  }))
+  this.setState((state) => ({
+    want: state.books.filter((b) => b.id !== book.id && b.shelf === "wantToRead")
+  }))
+  this.setState((state) => ({
+    read: state.books.filter((b) => b.id !== book.id && b.shelf === "read")
+  }))
+}
+  // this.setState((state) => ({
+  //   current: state.current.filter((b) => b.shelf !== book.shelf)
+  // }))
+  // this.setState((state) => ({
+  //   want: state.books.filter((b) => b.shelf === "wantToRead")
+  // }))
+  // this.setState((state) => ({
+  //   read: state.books.filter((b) => b.shelf === book.shelf)
+  // }))
+
+  BooksAPI.update(book, shelf)
+
+
+  // if (shelf === "currentlyReading") {
+  // this.setState((state) => ({
+  //   read: state.read.filter((b) => b.id !== book.id)
+  // }))
+  // }
+  //
+  // console.log(book.shelf)
+}
+
+
+
   render() {
+
     return (
+
       <div className="app">
+
         {this.state.showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
@@ -96,40 +121,14 @@ class BooksApp extends React.Component {
               <h1>MyReads</h1>
             </div>
 
-
-              <Books books={books} />
-
-
-
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
+            <Books
+            stateChange={this.updateBook}
+            current={this.state.current}
+            want={this.state.want}
+            read={this.state.read}
+            />
 
 
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-
-                    </ol>
-                  </div>
-                </div>
-              </div>
-            </div>
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
--- src/Books.js
+++ src/Books.js
@@ -1,18 +1,57 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import * as BooksAPI from './BooksAPI'
 
 class Books extends Component {
+  static propTypes= {
+    // books: PropTypes.array.isRequired,
+    stateChange: PropTypes.func.isRequired
+  }
+
+//
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     shelf: ''
+//   };
+//
+//   this.handleChange = this.handleChange.bind(this);
+//
+// }
+//
+// handleChange(event) {
+//   this.setState({shelf: event.target.value});
+//   // return(this.shelf);
+//   console.log(this.state.shelf)
+// }
+
+handleChange1(event) {
+  let st = event.target.value
+  console.log(st);
+  return(st);
+
+}
+
+
   render() {
+    const {books, stateChange} = this.props
+
     return (
+      <div className="list-books-content">
+        <div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+            <div className="bookshelf-books">
       <ol className='books-grid'>
-        {this.props.books.map((book) => (
+        {this.props.current.map((book) => (
           <li key={book.id}>
           <div className="book">
             <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
             <div className="book-shelf-changer">
-              <select>
+            <select value={"move"} onChange={(e)=> {BooksAPI.update(book, this.handleChange1(e))}}>
                 <option value="move" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
+                <option value="currentlyReading" >Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
                 <option value="none">None</option>
@@ -20,13 +59,76 @@ class Books extends Component {
               </div>
               </div>
               <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.author}</div>
+              <div className="book-authors">{book.authors}</div>
             </div>
           </li>
         ))}
       </ol>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Want to Read</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+          {this.props.want.map((book) => (
+            <li key={book.id}>
+            <div className="book">
+              <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-shelf-changer">
+              <select value={"move"} onChange={(e)=> {BooksAPI.update(book, this.handleChange1(e))}}>
+                <option value="move" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+                </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+              </div>
+            </li>
+          ))}
+          </ol>
+        </div>
+      </div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Read</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+          {this.props.read.map((book) => (
+            <li key={book.id}>
+            <div className="book">
+              <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+              <div className="book-shelf-changer">
+                <select value={"move"} onChange={(e) => {stateChange(book, this.handleChange1(e))}}>
+                  <option value="move" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+                </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+              </div>
+            </li>
+          ))}
+          </ol>
+
+        </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+
     )
   }
 }
 
 export default Books
+
+
+// () => onDeleteContact(contact)}
